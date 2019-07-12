5c29290042fa93e6f48366f16b5b7eb4b42413fe
Walker Floyd <walkerkeatley@yahoo.com>
2019-07-12 16:58:14 -0700

    tagged objects and collision

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -22,6 +22,20 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Ok!");
+                break;
+
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
     private void Rotate()
     {
         rigidBody.freezeRotation = true;
